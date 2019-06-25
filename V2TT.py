42a0eea3c885502c30b79a208f09cbb58b4f9845
nindanaoto <dirty.knife603@gmail.com>
2019-06-25 13:15:47 +0000

    Added view_pygraphviz for debugging

--- V2TT.py
+++ V2TT.py
@@ -7,13 +7,28 @@ CircuitGraph = nx.DiGraph()
 CircuitGraph.add_node("In")
 CircuitGraph.add_node("Out")
 
-json_netlist = json.load(open(sys.argv[1], 'r'))
-ports_netlist = json_netlist["modules"]["ports"]
-for port_json in ports_netlist.values():
-    if port_json["direction"] == "input":
-        CircuitGraph.add_edge("In",port_json["bits"][0])
-    if port_json["direction"] == "output":
-        CircuitGraph.add_edge("Out",port_json["bits"][0])
-    else:
-        print("Port Definition Error")
-        sys.exit(1)
+json_netlist = json.load(open(sys.argv[1], 'r'))["modules"]
+
+for module_netlist in json_netlist.values():
+    for port_json in module_netlist["ports"].values():
+        if port_json["direction"] == "input":
+            CircuitGraph.add_edge("In",port_json["bits"][0],weight = 0)
+        elif port_json["direction"] == "output":
+            CircuitGraph.add_edge(port_json["bits"][0],"Out",weight = 0)
+        else:
+            print("Port Definition Error")
+            sys.exit(1)
+
+    for cell_json in module_netlist["cells"].items():
+        CircuitGraph.add_node(cell_json[0],tyep = cell_json[1]["type"][2:])
+
+        for direction_json in cell_json[1]["port_directions"].items():
+            if direction_json[1] == "input":
+                CircuitGraph.add_edge(cell_json[1]["connections"][direction_json[0]][0],cell_json[0],weight = -1)
+            elif direction_json[1] == "output":
+                CircuitGraph.add_edge(cell_json[0],cell_json[1]["connections"][direction_json[0]][0],weight = -1)
+            else:
+                print("Cell Port Definition Error")
+                sys.exit(1)
+
+nx.nx_agraph.view_pygraphviz(CircuitGraph, prog='fdp') 
