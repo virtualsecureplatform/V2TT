69afb90f79da14b73803687d68a6fe2b7b22882c
nindanaoto <dirty.knife603@gmail.com>
2020-04-25 13:33:32 +0900

    Added Divider8bit test

---
+++ test/Divider8bit/Division8bit/client.cpp
@@ -0,0 +1,55 @@
+#include <tfhe/tfhe.h>
+#include <tfhe/tfhe_io.h>
+#include <stdio.h>
+#include <iostream>
+
+int main() {
+    //generate a keyset
+    const int minimum_lambda = 110;
+    TFheGateBootstrappingParameterSet* params = new_default_gate_bootstrapping_parameters(minimum_lambda);
+
+    //generate a random key
+    uint32_t seed[] = { 314, 1592, 657 };
+    tfhe_random_generator_setSeed(seed,3);
+    TFheGateBootstrappingSecretKeySet* key = new_random_gate_bootstrapping_secret_keyset(params);
+
+    //export the secret key to file for later use
+    FILE* secret_key = fopen("secret.key","wb");
+    export_tfheGateBootstrappingSecretKeySet_toFile(secret_key, key);
+    fclose(secret_key);
+
+    //export the cloud key to a file (for the cloud)
+    FILE* cloud_key = fopen("cloud.key","wb");
+    export_tfheGateBootstrappingCloudKeySet_toFile(cloud_key, &key->cloud);
+    fclose(cloud_key);
+   
+    //generate encrypt the input
+    uint32_t plaintext = 0;
+    std::cout << "Input A is" <<std::endl;
+    std::cin >> plaintext;
+    LweSample* ciphertext = new_gate_bootstrapping_ciphertext_array(18, params);
+    bootsSymEncrypt(&ciphertext[0], 0, key);
+    bootsSymEncrypt(&ciphertext[1], 0, key);
+    for (int i=0; i<8; i++) {
+        bootsSymEncrypt(&ciphertext[i+2], (plaintext>>i)&1, key);
+    }
+
+    std::cout << "Input B is" <<std::endl;
+    std::cin >> plaintext;
+
+    for (int i=0; i<8; i++) {
+        bootsSymEncrypt(&ciphertext[i+10], (plaintext>>i)&1, key);
+    }
+
+    //export the 6 ciphertexts to a file (for the cloud)
+    FILE* cloud_data = fopen("cloud.data","wb");
+    for (int i=0; i<18; i++) 
+        export_gate_bootstrapping_ciphertext_toFile(cloud_data, &ciphertext[i], params);
+    fclose(cloud_data);
+
+    //clean up all pointers
+    delete_gate_bootstrapping_ciphertext_array(18, ciphertext);
+    delete_gate_bootstrapping_secret_keyset(key);
+    delete_gate_bootstrapping_parameters(params);
+
+}
---
+++ test/Divider8bit/Division8bit/verify.cpp
@@ -0,0 +1,35 @@
+#include <tfhe/tfhe.h>
+#include <tfhe/tfhe_io.h>
+#include <stdio.h>
+#include <iostream>
+int main() {
+
+    //reads the cloud key from file
+    FILE* secret_key = fopen("secret.key","rb");
+    TFheGateBootstrappingSecretKeySet* key = new_tfheGateBootstrappingSecretKeySet_fromFile(secret_key);
+    fclose(secret_key);
+ 
+    //if necessary, the params are inside the key
+    const TFheGateBootstrappingParameterSet* params = key->params;
+
+    //read the 32 ciphertexts of the result
+    LweSample* answer = new_gate_bootstrapping_ciphertext_array(8, params);
+
+    //import the 8 ciphertexts from the answer file
+    FILE* answer_data = fopen("answer.data","rb");
+    for (int i=0; i<8; i++) 
+        import_gate_bootstrapping_ciphertext_fromFile(answer_data, &answer[i], params);
+    fclose(answer_data);
+
+    //decrypt and print plaintext answer
+    int32_t int_answer = 0;
+    for (int i=0; i<8; i++) {
+        int ai = bootsSymDecrypt(&answer[i], key);
+        int_answer |= (ai<<i);
+    }
+    std::cout << "Answer is" << std::endl << int_answer << std::endl;
+
+    //clean up all pointers
+    delete_gate_bootstrapping_ciphertext_array(8, answer);
+    delete_gate_bootstrapping_secret_keyset(key);
+}
